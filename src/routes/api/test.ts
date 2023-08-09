eca6a6b0affc90048f86260d8b1f1da62070e936
Birk Skyum <birk.skyum@pm.me>
2023-08-09 02:40:24 +0200

    simplify

--- src/routes/api/test.ts
+++ src/routes/api/test.ts
@@ -1,30 +1,18 @@
-import { storage } from "../../components/session.js";
 
 export async function GET({ request }: { request: Request }) {
-  
-  const session = await storage.getSession();
-  session.set("userInfo", "testdata");
-
-
-  session.set("idToken", "testdata");
-  session.set("scope", "testdata");
-  session.set("tokenType", "testdata");
-  session.set("userInfo", "testdata");
-  session.set("userInfo", "testdata");
-  session.set("userId", "testdata");
-
-
-  const headers = new Headers();
-  headers.append("Content-Type", "application/json");
-
-  headers.append("Set-Cookie", await storage.commitSession(session));
+  // const navigate = useNavigate();
 
   const body = `<html>
   <head>
+    <meta http-equiv="refresh" content="0;" />
   </head>
   <body></body>
 </html>`;
 
-  return new Response(body, { headers });
-
+  return new Response(body,{
+      headers: {
+        "Set-Cookie": 'test',
+      },
+    }
+  );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
+import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
-  plugins: [solid()],
+  server: { https: true, host: "127.0.0.1", port: 3000 },
+  plugins: [
+    mkcert(),
+    solid({
+      ssr: true,
+    }),
+  ]
 });
